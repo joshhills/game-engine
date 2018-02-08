d81f5fba651bd534e4104d2921ed1b52551424b7
Josh Hills <josh.hills@ymail.com>
2018-02-08 12:57:55 +0000

    Addded graphics middleware

---
+++ Game/Game/main.cpp
@@ -0,0 +1,7 @@
+#include <nclgl\Window.h>
+
+int main() {
+	Window w = Window(100, 100);
+
+
+}
---
+++ Game/Graphics/Renderer.cpp
@@ -0,0 +1,50 @@
+#include "Renderer.h"
+
+Renderer::Renderer(Window &parent) : OGLRenderer(parent)	{
+
+	glEnable(GL_DEPTH_TEST);
+}
+
+Renderer::~Renderer(void)	{
+	 
+}
+
+void	Renderer::RenderScene() {
+	for(vector<RenderObject*>::iterator i = renderObjects.begin(); i != renderObjects.end(); ++i ) {
+		Render(*(*i));
+	}
+}
+
+void	Renderer::Render(const RenderObject &o) {
+	modelMatrix = o.GetWorldTransform();
+
+	if(o.GetShader() && o.GetMesh()) {
+		GLuint program = o.GetShader()->GetShaderProgram();
+		
+		glUseProgram(program);
+
+		UpdateShaderMatrices(program);
+
+		o.Draw();
+	}
+
+	for(vector<RenderObject*>::const_iterator i = o.GetChildren().begin(); i != o.GetChildren().end(); ++i ) {
+		Render(*(*i));
+	}
+}
+
+void	Renderer::UpdateScene(float msec) {
+	for(vector<RenderObject*>::iterator i = renderObjects.begin(); i != renderObjects.end(); ++i ) {
+		(*i)->Update(msec);
+	}
+}
+
+GLuint Renderer::LoadTexture(const string& filename) {
+	GLuint texName = SOIL_load_OGL_texture(filename.c_str(), SOIL_LOAD_AUTO,SOIL_CREATE_NEW_ID, SOIL_FLAG_MIPMAPS);
+
+	if (texName == 0) {
+		cout << "Texture file " << filename << " failed to load!" << std::endl;
+	}
+
+	return texName;
+}
