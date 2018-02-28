b616a0caa9184651bfc2246b9c85b5ddca5befc7
Josh Hills <josh.hills@ymail.com>
2018-02-28 13:51:38 +0000

    Fixed include errors preventing build

--- Game/Physics/Physics.cpp
+++ Game/Physics/Physics.cpp
@@ -16,20 +16,20 @@ void Physics::CheckForEvents()
 	cout << "Checking for messages in physics subsystem." << endl;
 
 	// Search event queue for messages that pertain to this sub-system.
-	vector<Event *> eventQueue = eventManager->GetEventQueue();
-	for (int i = 0; i < eventQueue.size; i++) {
-		Event * e = eventQueue.at(i);
-		vector<Event::Subsystem> subsystems = e->subsystems;
-		
-		if (find(subsystems.begin(), subsystems.end(), Event::Subsystem::PHYSICS) != subsystems.end) {
-			HandleEvent(e);
-		}
-	}
+	//vector<Event *> eventQueue = eventManager->GetEventQueue();
+	//for (int i = 0; i < eventQueue.size; i++) {
+	//	Event * e = eventQueue.at(i);
+	//	vector<Event::Subsystem> subsystems = e->subsystems;
+	//	
+	//	if (find(subsystems.begin(), subsystems.end(), Event::Subsystem::PHYSICS) != subsystems.end()) {
+	//		HandleEvent(e);
+	//	}
+	//}
 }
 
 void Physics::HandleEvent(Event * e) {
-	switch (e->type) {
-	case Event::EventType::MOVE_UP:
-		
-	}
+	//switch (e->type) {
+	//case Event::EventType::MOVE_UP:
+	//	
+	//}
 }
